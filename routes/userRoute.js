96090d40b8d45f4e6ce021872ff14915f88303df
Ammar Ahmed <ammarahmed8088@gmail.com>
2024-04-09 11:34:06 +0500

    second commit

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,16 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const validateToken = require("../middlewares/validateToken");
 const {
   registerUser,
   loginUser,
   currentUser,
 } = require("../controller/userController");
-const validateToken = require("../middlewares/validateToken");
 
 router.route("/signin").post(registerUser);
 router.route("/login").post(loginUser);
-// router.post("/signin", registerUser);
-// router.post("/login", loginUser);
 router.get("/current", validateToken, currentUser);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,21 +3,22 @@ const app = express();
 const connectDb = require("./db/connectDb");
 const errorHandler = require("./middlewares/errorHandler");
 const userRoute = require("./routes/userRoute");
-const validateToken = require("./middlewares/validateToken");
 require("dotenv").config();
+const bodyParser = require("body-parser");
+
 const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
 app.use(express.json());
-bodyParser = require("body-parser");
 app.use(bodyParser.json());
-const crypto = require("crypto");
+
 const Start = async () => {
+  app.use("/user", userRoute);
+  app.use(errorHandler); // define below the routes
   app.listen(PORT, () => {
     console.log("server is listening at port ", PORT);
   });
-  await connectDb(process.env.DATABASE_URL);
-  console.log("successfully connected");
+  await connectDb(DATABASE_URL);
+
   // app.use(validateToken);
-  app.use("/user", userRoute);
-  app.use(errorHandler); // define below the routes
 };
 Start();
